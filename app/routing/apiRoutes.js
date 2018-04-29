c9571018add3ed584dd0d67c3c9e76184e433905
Miriam Armada <miriam_armada@yahoo.com>
2018-04-29 00:41:05 -0700

    final

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -23,10 +23,42 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.post('/api/friends', function(req, res) {
+  
+  var myBestFriend = {  
 
+    name:"",
+    picture: "",
+    scoreDelta: 99999
+  }
 
+  var newFriend= req.body;
+  var friendName= newFriend.name;
+  var picture = newFriend.photo;
+  var score = newFriend.scores;
+
+  var difference = 0;
+
+  //loop thru existing "Friends" & calculate delta score
+
+  for(var i=0 ; i < user.length; i++){
+
+    difference=0
+
+    for(var j=0; j< 10; j++){
+
+      difference+= Math.abs(parseInt(score[j])- parseInt(user[i].scores[j]));
+
+      if(difference <= myBestFriend.scoreDelta){
+
+        myBestFriend.name = user[i].name;
+        myBestFriend.picture = user[i].photo;
+        myBestFriend.scoreDelta = difference;
+      }
+
+    }
+  }
     user.push(req.body);
-    res.json(true);
+    res.json(myBestFriend);
   });
 
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -19,9 +19,11 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
-
   
   app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
